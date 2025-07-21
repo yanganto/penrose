7a8d638e6226bcc63829d2c5fbed367409643f23
Antonio Yang <yanganto@gmail.com>
2025-07-21 22:01:03 +0800

    add shell

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell ({
+            buildInputs = with pkgs; [ 
+              rust-bin.stable.latest.minimal
+              xorg.libX11
+              xorg.libXft
+              pkg-config
+            ];
+          });
+        };
+      }
+    );
+}
